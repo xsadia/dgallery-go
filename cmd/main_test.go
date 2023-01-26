7036529e3a854379099a61bc93e6d86ff75868f1
xsadia <felipe.rosa.arthur27@gmail.com>
2023-01-26 01:17:32 -0300

    feat(init) :monkey:

---
+++ cmd/main_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	exitVal := m.Run()
+
+	os.Exit(exitVal)
+}
+
+func TestRouter(t *testing.T) {
+	t.Run("returns pong", func(t *testing.T) {
+
+		expected := "pong"
+
+		app := NewServer()
+
+		req := httptest.NewRequest("GET", "/ping", nil)
+
+		resp, err := app.HTTP.Test(req, 1)
+
+		if err != nil {
+			t.Errorf("Expected error to be nil, got %s", err.Error())
+		}
+
+		var m map[string]string
+		body, err := io.ReadAll(resp.Body)
+
+		if err != nil {
+			t.Errorf("Expected error to be nil, got %s", err.Error())
+		}
+
+		json.Unmarshal(body, &m)
+
+		if resp.StatusCode != http.StatusOK {
+			t.Errorf("Expected %d, got %d", http.StatusOK, resp.StatusCode)
+		}
+
+		if m["data"] != expected {
+			t.Errorf("Expected %s, got %s", expected, m["message"])
+		}
+	})
+}
