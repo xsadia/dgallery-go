9dd8c66cb789e83f91a847131c0afa0f0bdae079
Felipe Arthur <felipe.rosa.arthur27@gmail.com>
2023-02-12 12:32:46 -0300

    feat(config): add env vars (#4)
    * feat(config): add env vars
    
    * feat(config): make EnvKeys a slice
    
    * test(config): change expect message
    
    * feat(config): add argument to Init function

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,13 @@
 package main
 
-import "github.com/xsadia/kgallery/pkg/server"
+import (
+	"github.com/xsadia/kgallery/config"
+	"github.com/xsadia/kgallery/pkg/server"
+)
 
 func main() {
 	app := server.NewServer()
+	config.Init("../.env")
 
 	app.ListenAndServe()
 }
